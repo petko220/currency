e86fa98fabb2d1c44e40be46600b1eaa4ab4e6d8
petko220 <128742231+petko220@users.noreply.github.com>
2023-12-14 10:21:42 +0200

    working settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,33 @@ import { Currency } from 'src/types/currency';
 })
 export class AppComponent {
   title = 'currency';
-  allCurrency:Currency[] = [];
+  allCurrency: Currency[] = [];
   currentId = 0;
   createHidden = true;
   editHidden = true;
   indexToEdit = 0;
   filteredCurrency: Currency[] = [];
+  areFiltered = false;
+  settingsShown = false;
+  filterIsShown = false;
+
+  tableColumns = {
+    active: true,
+    currencyId: true,
+    country: true,
+    currencyName: true,
+    currencyCode: true,
+    currencySymbol: true,
+    branch: true,
+    createdOn: true,
+    createdBy: true,
+    lastChanged: true,
+    lastChangedBy: true
+  };
 
 
   create(form: NgForm) {
-    const value:Currency = form.value;
+    const value: Currency = form.value;
     value.createdBy = 'Venelin Todorov';
     value.currencyId = this.currentId;
     this.currentId++;
@@ -29,21 +46,21 @@ export class AppComponent {
     this.allCurrency.push(value);
   }
 
-  delete(id:number) {
+  delete(id: number) {
     const result = this.allCurrency.find(currency => currency.currencyId === id);
-    const index = this.allCurrency.findIndex(currency => result?.currencyId === currency.currencyId );
+    const index = this.allCurrency.findIndex(currency => result?.currencyId === currency.currencyId);
     this.allCurrency.splice(index, 1);
   }
 
-  findOneToEdit(id:number) {
+  findOneToEdit(id: number) {
     this.editHidden = false;
     const result = this.allCurrency.find(currency => currency.currencyId === id);
-    const index = this.allCurrency.findIndex(currency => result?.currencyId === currency.currencyId );
+    const index = this.allCurrency.findIndex(currency => result?.currencyId === currency.currencyId);
     this.indexToEdit = index;
   }
-  
+
   edit(form: NgForm) {
-    const value:Currency = form.value;
+    const value: Currency = form.value;
     const currencyToEdit = this.allCurrency[this.indexToEdit];
     currencyToEdit.currencyName = value.currencyName
     currencyToEdit.currencyCode = value.currencyCode
@@ -51,13 +68,108 @@ export class AppComponent {
     currencyToEdit.branch = value.branch
     currencyToEdit.active = value.active
     value.lastChanged = new Date().toDateString();
-    
+
   }
 
-  filter(form:NgForm) {
-    const value:Currency = form.value;
+  filter(form: NgForm) {
+    const value = form.value.filterInput;
     console.log(value);
-    
+    this.filteredCurrency = this.allCurrency.filter(currency => currency.currencyCode == value);
+    console.log(this.filteredCurrency);
+    this.areFiltered = true;
+  }
+
+  removeColumn(columnName: string) {
+    switch (columnName) {
+      case 'active':
+        if (this.tableColumns["active"] === true) {
+          this.tableColumns["active"] = false
+        } else {
+          this.tableColumns["active"] = true
+        }
+        break;
+
+      case 'currencyId':
+        if (this.tableColumns["currencyId"] === true) {
+          this.tableColumns["currencyId"] = false
+        } else {
+          this.tableColumns["currencyId"] = true
+        }
+        break;
+
+      case 'country':
+        if (this.tableColumns["country"] === true) {
+          this.tableColumns["country"] = false
+        } else {
+          this.tableColumns["country"] = true
+        }
+        break;
+
+      case 'currencyName':
+        if (this.tableColumns["currencyName"] === true) {
+          this.tableColumns["currencyName"] = false
+        } else {
+          this.tableColumns["currencyName"] = true
+        }
+        break;
+
+      case 'currencyCode':
+        if (this.tableColumns["currencyCode"] === true) {
+          this.tableColumns["currencyCode"] = false
+        } else {
+          this.tableColumns["currencyCode"] = true
+        }
+        break;
+
+      case 'currencySymbol':
+        if (this.tableColumns["currencySymbol"] === true) {
+          this.tableColumns["currencySymbol"] = false
+        } else {
+          this.tableColumns["currencySymbol"] = true
+        }
+        break;
+
+      case 'branch':
+        if (this.tableColumns["branch"] === true) {
+          this.tableColumns["branch"] = false
+        } else {
+          this.tableColumns["branch"] = true
+        }
+        break;
+
+      case 'createdOn':
+        if (this.tableColumns["createdOn"] === true) {
+          this.tableColumns["createdOn"] = false
+        } else {
+          this.tableColumns["createdOn"] = true
+        }
+        break;
+
+      case 'createdBy':
+        if (this.tableColumns["createdBy"] === true) {
+          this.tableColumns["createdBy"] = false
+        } else {
+          this.tableColumns["createdBy"] = true
+        }
+        break;
+
+      case 'lastChanged':
+        if (this.tableColumns["lastChanged"] === true) {
+          this.tableColumns["lastChanged"] = false
+        } else {
+          this.tableColumns["lastChanged"] = true
+        }
+        break;
+
+      case 'lastChangedBy':
+        if (this.tableColumns["lastChangedBy"] === true) {
+          this.tableColumns["lastChangedBy"] = false
+        } else {
+          this.tableColumns["lastChangedBy"] = true
+        }
+        break;
+    }
+
   }
 
 }
