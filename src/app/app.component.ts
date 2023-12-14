deff891f332f19a7bab533bb6a7df9cd8b033aac
petko220 <128742231+petko220@users.noreply.github.com>
2023-12-14 06:47:56 +0200

    add + remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Currency } from 'src/types/currency';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'currency';
+  allCurrency:Currency[] = [];
+  currentId = 0;
+  createHidden = true;
+  editHidden = true;
+  currentEdit = {};
+
+  create(form: NgForm) {
+    const value:Currency = form.value;
+    value.createdBy = 'Venelin Todorov';
+    value.currencyId = this.currentId;
+    this.currentId++;
+    value.country = "Bulgaria";
+    value.createdOn = new Date().toDateString();
+    value.lastChangedBy = "Venelin Todorov";
+    value.lastChanged = new Date().toDateString();
+    this.allCurrency.push(value);
+  }
+
+  delete(id:number) {
+    const result = this.allCurrency.find(currency => currency.currencyId === id);
+    const index = this.allCurrency.findIndex(currency => result?.currencyId === currency.currencyId );
+    this.allCurrency.splice(index, 1);
+  }
+
+  findOneToEdit(id:number) {
+    this.editHidden = false;
+    const result = this.allCurrency.find(currency => currency.currencyId === id);
+    if(result) {
+      this.currentEdit = result
+      console.log(this.currentEdit);
+      
+    }
+  }
+  
+  edit(form: NgForm) {
+    const value:Currency = form.value;
+  }
+
 }
---
+++ src/types/currency.ts
@@ -0,0 +1,13 @@
+export interface Currency {
+    active: boolean
+    currencyId: number
+    country: string
+    currencyName: string
+    currencyCode: string
+    currencySymbol: string
+    branch: string
+    createdOn: string
+    createdBy: string
+    lastChanged: string
+    lastChangedBy: string
+}
